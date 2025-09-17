4d1736439300df2ae7a579d91f19ee09370436f3
Mohamed Zayed <mohamedzayed907@gmail.com>
2025-09-17 17:53:01 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,6 +4,9 @@ sudo useradd -s /bin/bash -d /home/ubuntu -m -G sudo ubuntu
 # make him a superuser without a password
 echo "ubuntu ALL=(ALL) NOPASSWD:AL" > /etc/sudoers.d/ubuntu
 
+# switch to ubuntu user
+su ubuntu
+
 # update os
 sudo apt update -y
 sudo apt upgrade -y
